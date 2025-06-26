75c60d9feb82daea592f767116f398726f6ad83b
Maxim <151841989+MaximSvid@users.noreply.github.com>
2025-06-26 13:45:53 +0200

    service - User Service

---
+++ src/main/java/com/example/event_management/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.event_management.service;
+
+import com.example.event_management.model.User;
+import com.example.event_management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Dieser Service bietet Methoden zur Verwaltung von Benutzern.
+ */
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    //Aktualisiert die Benutzerdaten anhand der ID
+    public Optional<User> updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+           user.setUserName(updatedUser.getUserName());
+           user.setEmail(updatedUser.getEmail());
+           user.setRole(updatedUser.getRole());
+           return userRepository.save(user);
+        });
+    }
+}
